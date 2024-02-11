a2cfd399776074223da8094be4ca30e905f1b37f
yankareddy <reddy.a111friend@gmail.com>
2024-02-11 19:36:14 +0530

    time-converter

---
+++ Time Converter.js
@@ -0,0 +1,19 @@
+let convertBtnEl = document.getElementById("convertBtn");
+
+convertBtnEl.addEventListener("click", function getSeconds() {
+    let hoursInputEl = parseInt(document.getElementById("hoursInput").value)
+    let minutesInputEl = parseInt(document.getElementById("minutesInput").value)
+    let seconds = (hoursInputEl) * 60 + (minutesInputEl) * 60
+
+    let errMsgEl = document.getElementById("errorMsg")
+    let showSeconds = document.getElementById("timeInSeconds")
+
+    if (isNaN(hoursInputEl) || isNaN(minutesInputEl)) {
+        errMsgEl.textContent = "please enter any value";
+        errMsgEl.style.color = "#f7faf5";
+    } else {
+        showSeconds.textContent = seconds;
+        showSeconds.style.color = "#ffffff";
+    }
+
+})
